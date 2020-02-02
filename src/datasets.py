535775589ff7a50eb4d4f9d0eee11d32fb7111e1
Ilya Trushkin <iatrushkin@edu.hse.ru>
2020-02-02 10:50:42 +0300

    Add submission

--- src/datasets.py
+++ src/datasets.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 from torchvision.transforms import Resize, ToTensor, Compose
 import torch
 from PIL import Image
+import re
 
 
 class MfsdDataset(Dataset):
@@ -40,13 +41,22 @@ class MfsdDataset(Dataset):
 
 
 class CasiaSurfDataset(Dataset):
-    def __init__(self, protocol: int, dir: str = 'data/CASIA_SURF', train: bool = True, ):
+    def __init__(self, protocol: int, dir: str = '../data/CASIA_SURF', train: bool = True, ):
         txt_metadata = 'train' if train else 'dev_res'
         self.dir = dir
         file_name = f'4@{protocol}_{txt_metadata}.txt'
         with open(os.path.join(dir, file_name), 'r') as file:
             lines = file.readlines()
-            self.items = [tuple(line[:-1].split(' ')) for line in lines]
+            if train:
+                self.items = [tuple(line[:-1].split(' ')) for line in lines]
+            else:
+                self.items = []
+                for line in lines:
+                    dir_name = os.path.join(line[:-1], 'profile')
+                    for file_name in os.listdir(os.path.join(dir, dir_name)):
+                        item = os.path.join(dir_name, file_name)
+                        self.items.append((item, -1))
+
         self.transform = Compose([Resize((320, 240)), ToTensor()])
 
     def __len__(self):
@@ -63,3 +73,7 @@ class CasiaSurfDataset(Dataset):
         img = self.transform(img)
 
         return img, int(label)
+
+    def get_video_id(self, idx: int):
+        img_name = self.items[idx][0]
+        return re.search(r'(?P<id>dev/\d+)', img_name).group('id')

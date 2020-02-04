9fbe4ff17132f6450deb7064a9e9ba3f45587b1b
trushkin-ilya <viciousde@gmail.com>
2020-02-05 01:08:29 +0300

    Add saving interval to train script parameter

--- src/train.py
+++ src/train.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     argparser.add_argument('--save_path', type=str, required=True)
     argparser.add_argument('--lr', type=float, default=3e-3)
     argparser.add_argument('--num_classes', type=int, default=2)
+    argparser.add_argument('--save_every', type=int, default=1)
     args = argparser.parse_args()
     dataset = CasiaSurfDataset(
         args.protocol, transform=transforms.Resize((320, 240)))
@@ -56,9 +57,10 @@ if __name__ == '__main__':
                 f'Epoch: {epoch + 1}/{args.epochs}\tBatch: {i + 1}/{len(train_loader)}\tLoss: {loss.item()}')
             loss.backward()
             optimizer.step()
-        file_name = f'mobilenet_v2_protocol{args.protocol}({epoch}).pt'
-        os.makedirs(args.save_path, exist_ok=True)
-        torch.save(model.state_dict, os.path.join(args.save_path, file_name))
+        if epoch % args.save_every == 0 and epoch > 0:
+            file_name = f'mobilenet_v2_protocol{args.protocol}({epoch}).pt'
+            os.makedirs(args.save_path, exist_ok=True)
+            torch.save(model.state_dict, os.path.join(args.save_path, file_name))
 
         if epoch % args.eval_every == 1:
             model.eval()

d1d522c1198f066bbad399a370e5935354198fc5
trushkin-ilya <viciousde@gmail.com>
2020-02-18 14:04:30 +0300

    Add visualization

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os
 import torch
+import numpy as np
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 from sklearn import metrics
@@ -10,12 +13,15 @@ from tqdm import tqdm
 from datasets import CasiaSurfDataset
 from utils import SplittedDataLoader
 
+classes = ('Spoof', 'Live')
 
-def evaluate(dataloader: data.DataLoader, model: nn.Module):
+
+def evaluate(dataloader: data.DataLoader, model: nn.Module, visualize: bool):
     device = next(model.parameters()).device
     model.eval()
     print("Evaluating...")
     tp, tn, fp, fn = 0, 0, 0, 0
+    errors = np.array([])
     with torch.no_grad():
         for i, batch in enumerate(tqdm(dataloader)):
             images, labels = batch
@@ -24,6 +30,11 @@ def evaluate(dataloader: data.DataLoader, model: nn.Module):
             tn_batch, fp_batch, fn_batch, tp_batch = metrics.confusion_matrix(y_true=labels,
                                                                               y_pred=torch.max(outputs.data, 1)[1],
                                                                               labels=[0, 1]).ravel()
+            if visualize:
+                errors_idx = np.where(torch.max(outputs.data, 1)[1] != labels)
+                errors_imgs = list(zip(images[errors_idx], labels[errors_idx]))
+                errors = np.append(errors, errors_imgs)
+
             tp += tp_batch
             tn += tn_batch
             fp += fp_batch
@@ -31,9 +42,60 @@ def evaluate(dataloader: data.DataLoader, model: nn.Module):
     apcer = fp / (tn + fp) if fp != 0 else 0
     bpcer = fn / (fn + tp) if fn != 0 else 0
     acer = (apcer + bpcer) / 2
+    if visualize:
+        dtype = [('img', object), ('label', np.uint), ('prob', float)]
+        errors = np.array(errors, dtype=dtype)
+        errors.sort(order='prob')
+        errors = np.flip(errors)
+        plot_classes_preds(model, *errors)
+
 
     return apcer, bpcer, acer
 
+
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+def images_to_probs(net: nn.Module, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.numpy())
+    return preds, [F.log_softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx + 1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+            color=("green" if preds[idx] == labels[idx].item() else "red"))
+    return fig
 
 if __name__ == '__main__':
     argparser = ArgumentParser()
@@ -42,6 +104,7 @@ if __name__ == '__main__':
     argparser.add_argument('--checkpoint', type=str, required=True)
     argparser.add_argument('--num_classes', type=int, default=2)
     argparser.add_argument('--batch_size', type=int, default=1)
+    argparser.add_argument('--visualize', type=bool, default=False)
     args = argparser.parse_args()
     dataset = CasiaSurfDataset(args.protocol, dir=args.data_dir, transform=transforms.Compose([
         transforms.Resize((320, 240)),
@@ -51,5 +114,5 @@ if __name__ == '__main__':
     model = models.mobilenet_v2(num_classes=args.num_classes)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.load_state_dict(torch.load(args.checkpoint, map_location=device))
-    apcer, bpcer, acer = evaluate(dataloader.val, model)
+    apcer, bpcer, acer = evaluate(dataloader.val, model, args.visualize)
     print(f'APCER: {apcer}, BPCER: {bpcer}, ACER: {acer}')

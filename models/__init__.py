ffb4e621b8063620cfca254728a232b6e3df04c7
trushkin-ilya <viciousde@gmail.com>
2020-05-07 23:09:35 +0300

    add dropout to resnet18

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .ensemble import Ensemble
 from .feathernet import FeatherNet
-from .SimpleBlock import ResNet18_Dropout
+from .resnet18_dropout import ResNet18_Dropout
 
 __all__ = ['Ensemble', 'FeatherNet', 'ResNet18_Dropout']

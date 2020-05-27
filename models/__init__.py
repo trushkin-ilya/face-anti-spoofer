641e124c8b08f413eb99c513686ee8217af78ad5
trushkin-ilya <viciousde@gmail.com>
2020-05-27 20:11:56 +0300

    extend model exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 from .ensemble import Ensemble
 from .feathernet import FeatherNet
 from .resnet18_dropout import ResNet18_Dropout
+from .mobilelitenet import MobileLiteNet, MobileLiteNet_se
 
-__all__ = ['Ensemble', 'FeatherNet', 'ResNet18_Dropout']
+
+__all__ = ['Ensemble', 'FeatherNet', 'ResNet18_Dropout',
+           'MobileLiteNet', 'MobileLiteNet_se']

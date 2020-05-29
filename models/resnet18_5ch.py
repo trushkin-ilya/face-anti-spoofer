8557298586a02799592bdb8fcc5bc77e018c9df1
trushkin-ilya <viciousde@gmail.com>
2020-05-29 19:54:02 +0300

    fix channels in conv layer

--- models/resnet18_5ch.py
+++ models/resnet18_5ch.py
@@ -3,5 +3,5 @@ from torch import nn
 
 def resnet18_5ch(**kwargs):
     model = resnet18(**kwargs)
-    model.conv1 = nn.Conv2d(5, model.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+    model.conv1 = nn.Conv2d(5, 64, kernel_size=7, stride=2, padding=3, bias=False)
     return model

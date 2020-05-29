5e2ac9a45d3d50d7230c2f99b512620467d2fa53
trushkin-ilya <viciousde@gmail.com>
2020-05-29 20:10:50 +0300

    add channels to resnet with dropouts

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 from .ensemble import Ensemble
 from torchvision.models import *
 from .feathernet import FeatherNetA, FeatherNetA_5ch
-from .resnet18_dropout import ResNet18_Dropout
+from .resnet18_dropout import ResNet18_Dropout, Resnet18_Dropout_5ch
 from .mobilelitenet import MobileLiteNet54, MobileLiteNet54_5ch
 from .resnet18_5ch import resnet18_5ch
 
 
-__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch', 'resnet18_5ch']
+__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch', 'resnet18_5ch', 'Resnet18_Dropout_5ch']

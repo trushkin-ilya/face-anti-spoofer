68e606f156fd16f406d48c0d9bfe13f14d2effa6
trushkin-ilya <viciousde@gmail.com>
2020-05-29 19:46:20 +0300

    add 5 channels to resnet

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from torchvision.models import *
 from .feathernet import FeatherNetA, FeatherNetA_5ch
 from .resnet18_dropout import ResNet18_Dropout
 from .mobilelitenet import MobileLiteNet54, MobileLiteNet54_5ch
+from .resnet18_5ch import resnet18_5ch
 
 
-__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch']
+__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch', 'resnet18_5ch']

c67d6c483cc0bcd4213672bb6d33f26576c63fe9
trushkin-ilya <viciousde@gmail.com>
2020-06-06 19:50:00 +0300

    add simple block

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from .feathernet import FeatherNetA, FeatherNetA_5ch
 from .resnet18_dropout import ResNet18_Dropout, Resnet18_Dropout_5ch
 from .mobilelitenet import MobileLiteNet54, MobileLiteNet54_5ch
 from .resnet18_5ch import resnet18_5ch
+from .simple_block import SimpleBlock
 
 
-__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch', 'resnet18_5ch', 'Resnet18_Dropout_5ch']
+__all__ = ['Ensemble', 'FeatherNetA', 'ResNet18_Dropout', 'MobileLiteNet54', 'MobileLiteNet54_5ch', 'FeatherNetA_5ch', 'resnet18_5ch', 'Resnet18_Dropout_5ch', 'SimpleBlock']
---
+++ models/simple_block.py
@@ -0,0 +1,25 @@
+import torch
+
+from torchvision.models.resnet import BasicBlock
+from torch import nn
+
+class SimpleBlock(nn.Module):
+    def __init__(self, in_ch=3, norm_layer=nn.BatchNorm2d, num_classes=2, ch=64):
+        super(SimpleBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_ch, ch, kernel_size=7, stride=2, padding=3,
+                           bias=False)
+        self.bn1 = norm_layer(ch)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer = BasicBlock(ch, ch)
+        self.fc = nn.Linear(ch * 56 * 56, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x

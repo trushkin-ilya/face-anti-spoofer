236b96a89283e7cad83b8452beaf12fdb2ef5764
trushkin-ilya <viciousde@gmail.com>
2020-06-10 10:04:48 +0300

    fix MobileLiteNet

--- models/mobilelitenet.py
+++ models/mobilelitenet.py
@@ -2,7 +2,7 @@ from baseline.models.MobileLiteNet import MobileLiteNet54
 from torch import nn
 
 
-class MobileLiteNet54_5ch(MobileLiteNet54):
-    def __init__(self, **kwargs):
-        super(MobileLiteNet54_5ch, self).__init__(**kwargs)
-        self.conv1 = nn.Conv2d(5, self.channels[0], kernel_size=3, stride=2, padding=1, bias=False)
+def MobileLiteNet54_5ch(**kwargs):
+    model = MobileLiteNet54(**kwargs)
+    model.conv1 = nn.Conv2d(5, model.channels[0], kernel_size=3, stride=2, padding=1, bias=False)
+    return model
